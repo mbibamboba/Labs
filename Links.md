ad884917523dad20f0e7e25a67283acd80a1cf61
R.Ruzana <ramazanova.ruzana@mail.ru>
2024-03-02 18:41:29 +0500

    Add run.py, Links.md

---
+++ Links.md
@@ -0,0 +1,8 @@
+# Источники
+1. https://ru.wikipedia.org/wiki/Git
+2. https://git-scm.com/
+3. https://git-scm.com/book/ru/v2/
+
+# Мемасик
+
+![](https://cs9.pikabu.ru/post_img/2019/09/30/10/1569865493165237986.jpg)
---
+++ run.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from tkinter import *  
+
+def clicked():
+    name=txt.get()[::-1]
+    lbname.configure(text=name) 
+  
+window = Tk()  
+window.title("Добро пожаловать в приложение PythonRu")  
+window.geometry('400x250')  
+lbl = Label(window, text="Введите свое имя")  
+lbl.grid(column=0, row=0)  
+lbname = Label(window, text='', height=3, font='Arial 30')
+lbname.grid(column=2, row=2)  
+txt = Entry(window,width=10)  
+txt.grid(column=1, row=0)  
+btn = Button(window, text="Нажмите!", command=clicked)  
+btn.grid(column=2, row=0)  
+window.mainloop()
